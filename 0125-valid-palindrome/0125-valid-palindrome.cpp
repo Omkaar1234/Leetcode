21cfc8d771c61cdee1c7610e1547e1758f684131
Omkar Kulkarni <omkarkk004@gmail.com>
2026-08-11 09:50:53 +0530

    Time: 1 ms (44.03%), Space: 9.9 MB (78.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.size();
         int l = 0;
+        int n = s.size();
         int r = n-1;
 
         while(l < r){
             while(l < r && !isalnum(s[l])) l++;
-
             while(l < r && !isalnum(s[r])) r--;
 
-            if(tolower(s[l]) != tolower(s[r])){
-                cout<<l<<" "<<r;
-                return false;
-            }
+            if(tolower(s[l]) != tolower(s[r])) return false;
             l++;
             r--;
         }
