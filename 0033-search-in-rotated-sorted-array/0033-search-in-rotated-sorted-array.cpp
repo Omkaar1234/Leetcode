434b01b0adadd4221d92e33c3be2587d2359538b
Omkar Kulkarni <omkarkk004@gmail.com>
2026-08-26 19:46:53 +0530

    Time: 0 ms (100%), Space: 15.4 MB (36.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,18 +8,19 @@ public:
         while(st <= end){
             int mid = st + (end-st)/2;
 
-            if(nums[mid] == target) return mid;
-            else if(nums[st] <= nums[mid]){ //Left array sorted
-                if(nums[st] <= target && nums[mid] > target){
-                    end = mid-1;
-                }else{
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] <= nums[end]){
+                if(nums[mid] < target && target <= nums[end]){
                     st = mid+1;
+                }else{
+                    end = mid-1;
                 }
-            }else if(nums[mid] <= nums[end]){ //Right Array sorted
-                if(nums[mid] < target && nums[end] >= target){
-                    st = mid+1;
-                }else{
+            }else if(nums[mid] >= nums[st]){
+                if(nums[st] <= target && target < nums[mid]){
                     end = mid-1;
+                }else{
+                    st = mid+1;
                 }
             }
         }
