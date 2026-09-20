09a77b20ee3013b3c70aa385b2b83846bd8213e9
Omkar Kulkarni <omkarkk004@gmail.com>
2026-09-20 13:57:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for(int i=0; i<s.size(); i++){
+            int idx = 'z' - s[i] + 1;
+            ans = ans + (idx * (i+1));
+        }
+
+        return ans;
+    }
+};
